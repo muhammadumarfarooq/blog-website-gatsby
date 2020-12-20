4dde1a0a6cb70e795bedab6ff43fac110f7e1524
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-12-20 19:05:50 +0500

    basic folder structure

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Layout = ({ className }) => {
+  return (
+    <div className={className}>
+    
+    </div>
+  );
+};
+
+Layout.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Layout;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+    
+    </nav>
+  );
+};
+
+export default Navbar;
