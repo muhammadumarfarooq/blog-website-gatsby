6bdac40a677de8971eb83082b17a2657b4f25ef2
Muhammad Umar Farooq <umar@orderspot.co>
2020-12-24 20:01:47 +0500

    configure firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,20 @@ module.exports = {
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: "sX0tfPm1XNaRkO-CTa8DgPpjw8G5jlqGLPZuKCBD0H0"
       }
-    }]
-}
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyAaZwY2i5rMJpVFhfimaPQ5DnnOjLdd2-I",
+          authDomain: "gatsby-contenful-blog.firebaseapp.com",
+          projectId: "gatsby-contenful-blog",
+          storageBucket: "gatsby-contenful-blog.appspot.com",
+          messagingSenderId: "65631998759",
+          appId: "1:65631998759:web:535897a003d98ce1240f02"
+        }
+      }
+    }
+  
+  ]
+};
