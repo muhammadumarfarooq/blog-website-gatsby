d6694ff2b765b7ed0afb50557ce87af955d0a6f4
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-12-25 21:30:12 +0500

    only show limited posts for logged in user

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import BlogSingle from "./BlogSingle";
+import UserContext from "../context/user/userContext";
 
 const Blogs = ({ posts }) => {
+  const { state } = useContext(UserContext);
+  const [allPosts, setAllPosts] = useState([]);
+  
+  useEffect(() => {
+    if ( !state.isUserLoggedIn ) {
+      setAllPosts(posts.slice(0, 4));
+    } else {
+      setAllPosts(posts);
+    }
+    
+  }, [state.isUserLoggedIn]);
   
   return (
     <section className="blogs">
       <div className="container">
         <div className="row">
-          {posts.map((post) => <BlogSingle key={post.node.slug} post={post}/>)}
+          {allPosts.map((post) => <BlogSingle key={post.node.slug} post={post}/>)}
         </div>
       </div>
+      {!state.isUserLoggedIn &&
+      <h1 className="blogs-helper--text">Please sign in to see all the available posts!</h1>}
+    
     </section>
   );
 };
